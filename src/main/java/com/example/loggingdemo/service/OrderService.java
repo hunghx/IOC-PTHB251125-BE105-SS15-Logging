a2361cfc9c38a23fe8f0a292c00dcc2674e50adc
hunghx <baby10051999@gmail.com>
2026-09-16 19:46:57 +0700

    update

---
+++ src/main/java/com/example/loggingdemo/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.example.loggingdemo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+//@Slf4j // tự sinh đối tượng logger
+public class OrderService {
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+    // cung cấp 1 phương thực tạo mới hóa đơn
+    public void createOrder() {
+        // tạo 1 mã định danh duy nhất cho hóa đơn
+        String orderId = UUID.randomUUID().toString();
+        // Để ghi log đc thì cần 1 đối tượng Logger
+        log.info("Tạo hóa đơn cho đơn hàng có OrderId là : {}", orderId);
+        log.warn("Tạo hóa đơn cho đơn hàng có OrderId là : {}", orderId);
+        log.error("Tạo hóa đơn cho đơn hàng có OrderId là : {}", orderId);
+        log.debug("Tạo hóa đơn cho đơn hàng có OrderId là : {}", orderId);
+        log.trace("Tạo hóa đơn cho đơn hàng có OrderId là : {}", orderId);
+    }
+}
